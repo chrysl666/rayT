eabbdf63d12c729ad2bfc8b555693d0da051a4d3
chrysl666 <chrysl666@gmail.com>
2012-12-13 16:00:59 -0200

    Qt base code

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+//base class for Application loop
+#include <QApplication>
+#include <QImage>
+#include <QLabel>
+#include "World.h"
+
+// base class for all kind of Qt widget
+#include <QtGui>
+
+int main(int argc , char *argv[])
+{      
+    
+    
+    World w;
+    w.build();
+    w.render_scene();
+    w.save_image();
+    
+    
+    
+        //start a main app instance
+    QApplication app(argc , argv);
+    QGraphicsView myView;
+    QGraphicsScene myscene;
+    myView.setScene(&myscene);
+    
+    
+    myscene.addPixmap( QPixmap::fromImage(w.getImage() ) );
+    
+    myView.show();
+    myView.update();
+
+    
+
+    
+    
+
+
+        // return and start the main application loop
+  //      return 0;
+    
+        return app.exec();
+
+
+}
+
